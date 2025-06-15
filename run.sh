ae532a9b66f530fad9a1369236d8f27bb9714611
ducto489 <dustnn00@gmail.com>
2025-06-15 10:31:34 +0700

    Update requirements and modify run script for dataset paths

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Set PYTHONPATH to include the current directory so that src module can be imported
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 python src/main.py \
     --local_dataset_path ./logs/datasets \
     --local_model_logs_path ./logs/training_logs \
     --local_pretrained_model_path ./logs/pretrained_models \
-    --gdrive_dataset_path po_gdrive:regent-fabric-training-repo/datasets \
-    --gdrive_model_logs_path po_gdrive:regent-fabric-training-repo/training_logs \
-    --gdrive_pretrained_model_path po_gdrive:regent-fabric-training-repo/pretrained_models
+    --gdrive_dataset_path anyglow:regent-fabric-training-repo/datasets \
+    --gdrive_model_logs_path anyglow:regent-fabric-training-repo/training_logs \
+    --gdrive_pretrained_model_path anyglow:regent-fabric-training-repo/pretrained_models
